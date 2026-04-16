1f4608677e7aec850cbab325616dea1bf8593df0
Fidele <nshutifidele1@gmail.com>
2026-04-16 10:03:44 +0000

    paste this code between the <head></head> tags on each page of your site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3044875222624730" crossorigin="anonymous"></script>
       </head>
       <body className="font-body antialiased bg-background text-foreground flex flex-col min-h-screen">
         <Header />
